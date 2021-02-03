04d482e97862febfea65f61e09c524623f9354e4
chaejin-jen <jen71324@gmail.com>
2021-02-03 18:13:08 +0900

    hkit project updated

---
+++ 03. Pedestrian_light_recognition/android/app/src/main/java/com/example/pedestrianlightapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.pedestrianlightapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://221.164.153.220:5000/");
+        webView.setWebViewClient(new WebViewClientClass());
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+}
